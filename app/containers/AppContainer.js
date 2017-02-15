8608edd9473b400eebf6dc675d78fa81d20048c4
SIlas Kwong <silaskwong@SIlass-MacBook-Pro-2.local>
2017-02-14 17:17:28 -0800

    pt5 complete, incrementor and logic in actions/reducers plumbed

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
-import {Text, View, AppRegistry, StyleSheet} from 'react-native'
+import {Text, View, AppRegistry, StyleSheet, TouchableHighlight} from 'react-native'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { ActionCreators } from '../actions'
 
-export default class AppContainer extends Component {
+class AppContainer extends Component {
+  addRecipe(){
+    this.props.addRecipe()
+  }
+
   render() {
     return <View>
       <Text style={{marginTop:20}}>
-        REKTM8
+        REKTM8 Recipe Count: {this.props.recipeCount}
       </Text>
+      <TouchableHighlight onPress={()=> {this.addRecipe() }}>
+        <Text>IncrementREKTNESS</Text>
+      </TouchableHighlight>
     </View>
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  
+  return bindActionCreators(ActionCreators, dispatch);
 }
+
+export default connect((state) => {
+  return {
+    recipeCount: state.recipeCount
+  }
+}, mapDispatchToProps)(AppContainer)
--- app/reducers/recipes.js
+++ app/reducers/recipes.js
@@ -1,5 +1,12 @@
 import createReducer from '../lib/createReducer'
 import * as types from '../actions/types'
 export const searchedRecipes = createReducer({}, {
-  
-})
+
+});
+
+//default value of zero to increment up.
+export const recipeCount = createReducer(0, {
+  [types.ADD_RECIPE](state, action) {
+    return state+1
+  }
+});
